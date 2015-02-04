3f9ee361b6e927eaba859441c9bd4b9491d82b11
andrewsharmon <aharmon256@gmail.com>
2015-02-04 01:15:52 -0600

    Add status check
    To verify system status add call to .getInfo()

--- Examples/BNO055_test/BNO055_test.ino
+++ Examples/BNO055_test/BNO055_test.ino
@@ -10,20 +10,25 @@
 
 void setup() 
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   
  
    if (!imu.begin(imu.NDOF, imu.NORMAL_MODE )){
     Serial.println("unable to initialize the chip, or it isnt ready. I'll give it a sec and try anyway!");
     delay(30);
   }
+ delay(1000);
+ imu.getInfo();
+ Serial.print("status code: ");Serial.println(imu.SystemStatusCode);
+ Serial.print("Selftest results: ");Serial.println(imu.SelfTestStatus);
+ Serial.print("System Error code: ");Serial.println(imu.SystemError);
 }
 
 void loop() 
 {
   imu.readEuler();
-  Serial.print("X: "); Serial.print((float)imu.EulerData.x);   Serial.print(" ");
-  Serial.print("Y: "); Serial.print((float)imu.EulerData.y);   Serial.print(" ");
-  Serial.print("Z: "); Serial.println((float)imu.EulerData.z); Serial.print(" ");
+  Serial.print("X: "); Serial.print((float)imu.EulerData.x,6);   Serial.print(" ");
+  Serial.print("Y: "); Serial.print((float)imu.EulerData.y,6);   Serial.print(" ");
+  Serial.print("Z: "); Serial.println((float)imu.EulerData.z,6); Serial.print(" ");
   delay(100);
 }
